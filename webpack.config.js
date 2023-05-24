e2b0f329e246305def48db79b9931d67502bb414
jungle77 <junglej77>
2023-05-24 10:22:41 +0800

    前端配置暂时先这样， 开始整理

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,31 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const glob = require('glob');
 
-let entries = {
+// JavaScript 入口配置
+let jsEntries = {
     'public/js/jungle_browse_statistics-public': './src/public/js/jungle_browse_statistics-public.js',
     'admin/js/jungle_browse_statistics-admin': './src/admin/js/jungle_browse_statistics-admin.js',
 };
-glob.sync('./src/*/*.less').forEach(filepath => {
-    let name = filepath.replace('.less', '');
-    entries[name] = filepath;
-});
-module.exports = {
+
+// CSS 入口配置
+let cssEntries = {
+    'public/css/jungle_browse_statistics-public': './src/public/less/jungle_browse_statistics-public.less',
+    'admin/css/jungle_browse_statistics-admin': './src/admin/less/jungle_browse_statistics-admin.less',
+};
+// let cssEntries = {};
+// const lessFiles = glob.sync('./src/**/*.less');
+// lessFiles.forEach((filepath) => {
+//     const entry = filepath.replace('/src/', '/').replace('.less', '');
+//     cssEntries[entry] = filepath;
+// });
+
+// JavaScript 配置
+const jsConfig = {
     mode: 'development',
-    entry: entries,
+    entry: jsEntries,
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname),
-        publicPath: '/',
     },
     module: {
         rules: [
@@ -32,15 +42,32 @@ module.exports = {
                     }
                 }
             },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+    },
+};
+
+// CSS 配置
+const cssConfig = {
+    mode: 'development',
+    entry: cssEntries,
+    output: {
+        filename: '[name].js',  // 这个输出的 .js 文件会是空的，你可以选择忽略它
+        path: path.resolve(__dirname),
+    },
+    module: {
+        rules: [
             {
                 test: /\.less$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../',  // 这里可以根据实际情况进行调整
-                        },
-                    },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'less-loader',
                 ],
@@ -50,17 +77,18 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: ({ chunk }) => {
-                return chunk.name.replace('src', '').replace('.js', '.css');
-            }
+                return `${chunk.name}.css`;
+            },
         }),
     ],
     optimization: {
         minimize: true,
         minimizer: [
             new CssMinimizerPlugin(),
-            new TerserPlugin({
-                extractComments: false,
-            }),
         ],
     },
 };
+
+// 使用多配置模式
+module.exports = [jsConfig, cssConfig];
+
