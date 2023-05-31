7446c077c8620e3e15a9122d94f5e4b8b76c8707
yousen <532337812@qq.com>
2023-05-31 16:33:28 +0800

    6S未发送心跳的，把离开时间更新

--- includes/service/class-web_socket_server.php
+++ includes/service/class-web_socket_server.php
@@ -1,58 +1,59 @@
 <?php
 include 'class-yousen_websocket.php';
 $k_cache_ip;
-$k_cache_leave_time;
 $config=array('address'=>'192.168.101.69',
   'port'=>'8088',
   'event'=>'WSevent', //回调函数的函数名
-  'log'=>true,
+  'log'=>false,
 );
 $websocket=new websocket($config);
 $websocket->run();
+checkTimeOut();
 
 function WSevent($type, $event) {
   global $websocket;
   global $k_cache_ip;
-  global $k_cache_leave_time;
 
   if('in'==$type) {
     $websocket->log('client in,id:'.$event['k']);
   }elseif('out'==$type) {
     $websocket->log('client out,id:'.$event['k']);
-    updateLeaveTime($event,$k_cache_ip);
+    updateLeaveTime($event['k'],$k_cache_ip);
   }elseif('msg'==$type) {
     $websocket->log($event['k'].'message:'.$event['msg']);
     handleMsg($event, $k_cache_ip);
   }elseif('timeout'==$type){
     $websocket->log('client timeout,id:'.$event['k']);
-    updateLeaveTime($event,$k_cache_ip);
+    updateLeaveTime($event['k'],$k_cache_ip);
   }
 }
 
 function handleMsg($event, $k_cache_ip) {
   global $websocket;
   global $k_cache_ip;
-  global $k_cache_leave_time;
   $cache_ip=$event['msg'];
-
+  $timestamp = time();
+  $value = array('ip'=>$cache_ip,'leave_time'=>$timestamp);
   if(empty($k_cache_ip)) {
     // 向Map中添加键值对  
     $k_cache_ip=array();
-    $value = array('ip'=>$cache_ip,'leave_time'=>time())
-    $k_cache_ip[$event['k']]=$cache_ip;
+    $k_cache_ip[$event['k']]=$value;
   }else {
-    $k_cache_ip[$event['k']]=$cache_ip;
+    $k_cache_ip[$event['k']]=$value;
   }
-  $websocket->write($event['sign'], 'done:'.$cache_ip.$event['k']);
   //校验是否有超时的，超时就移除
-
+    foreach ($k_cache_ip as $key => $value) {
+      if($value['leave_time']-$timestamp<-6){
+        updateLeaveTime($key,$k_cache_ip);
+      }
+    }
+  $websocket->write($event['sign'], 'done:'.$cache_ip.$event['k']);
 }
-
-function updateLeaveTime($event,$k_cache_ip){
+function updateLeaveTime($k,$k_cache_ip){
   global $k_cache_ip;
     // 检查某个键是否存在  
-    if (isset($k_cache_ip[$event['k']])) {
-      $cache_ip=$k_cache_ip[$event['k']];
+    if (isset($k_cache_ip[$k])) {
+      $cache_ip=$k_cache_ip[$k]['ip'];
           
     // 创建连接
     $conn = new mysqli("localhost", "yousentest", "siYaojing.748", "www_yousentest_com");
@@ -64,12 +65,11 @@ function updateLeaveTime($event,$k_cache_ip){
     $sql="UPDATE wp_jungle_statistics_pages_view SET leave_time='".$formatted_time."'
     WHERE cache_ip='".$cache_ip."'
     limit 1 ";
-    echo $sql;
     $result = $conn->query($sql);
     
     $conn->close();
       // 删除某个键值对  
-      unset($k_cache_ip[$event['k']]);
+      unset($k_cache_ip[$k]);
     }
 }
 ?>
