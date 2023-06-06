8aedc59f137cc526b06c98f936efd94a9aa5f74b
yousen <532337812@qq.com>
2023-06-06 17:40:58 +0800

    1.移除不用的函数 2.修改访问统计表

--- includes/class-jungle_browse_statistics-activator.php
+++ includes/class-jungle_browse_statistics-activator.php
@@ -14,9 +14,6 @@ class Jungle_browse_statistics_Activator
 		//创建当前页面在线人数数据option
 		add_option("jungle_browse_statistics_online_count",0);
 		
-   		//根据用户访问记录，去统计今天的访问情况
-    	statistics($k_cache_ip);
-		
 		//检查是否已经开启了定时任务
 		$jungle_browse_statistics_cron_flag = wp_schedule_event('jungle_browse_statistics_cron_hook');
 		//未开启就开启
@@ -33,7 +30,7 @@ function create_jungle_browse_statistics_cron(){
     // //首先创建时间间隔
     // add_filter( 'cron_schedules', 'jungle_browse_statistics_cron_interval' );
     add_action( 'jungle_browse_statistics_cron_hook', 'jungle_browse_statistics_cron_exec' );
-    //立刻执行，随后每300秒执行一次
+    //立刻执行，随后每天执行一次
     wp_schedule_event( time(), 'daily', 'jungle_browse_statistics_cron_hook' );
 }
 
@@ -59,14 +56,14 @@ function jungle_browse_statistics_cron_exec(){
 	where enter_time BETWEEN '${startTime}' AND '${endTime}'
 	GROUP BY cache_ip,current_page";
 	$result = $wpdb->query($querySql);
-	$insertSql = "insert into $table_name_pages_view (`cache_ip`,`view_page`,`view_time`) values";
+	$insertSql = "insert into $table_name_pages_view (`cache_ip`,`view_page`,`view_time`,`create_time`) values";
 	if ($result) {
 	// 查询到了记录
 	foreach ($wpdb->last_result as $post) {
 		$cache_ip=$post->cacha_ip;
 		$view_page=$post->current_page;
 		$view_time=$post->view_time;
-		$insertSql=$insertSql."(".$cache_ip.",".$view_page.",".$view_time."),";
+		$insertSql=$insertSql."(".$cache_ip.",".$view_page.",".$view_time.",".$startTime."),";
 	}
 	//把末尾多余的,删除
 	$arr = explode(",", $insertSql);
